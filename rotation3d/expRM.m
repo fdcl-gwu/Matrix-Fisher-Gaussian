482b0e6cdaad253847a255cede88970701d08d04
weixinwang <wwang442@wisc.edu>
2019-08-19 13:02:23 -0400

    some modifications of the rotation library

--- rotation3d/expRM.m
+++ rotation3d/expRM.m
@@ -1,19 +1,45 @@
-function [ R ] = expRM( V )
-% Converting rotation vector to rotation matrix (exponential map)
-% Reference: Joan Sola, Quaternion kinematics for the error-state Kalman filter, 2017
-% Last edited by Weixin Wang, December 18, 2018
+function [ R ] = expRM( S, checkSS )
+% calculate the exponetial map from so(3) to SO(3)
+% if size(V)=[3,3,n], treat V as n skew symmetric matrices;
+% if size(V)=[3,n,1] or size(V)=[n,3,1], treat V as n 3-vectors;
+% otherwise returns error.
+% if size(V)=[3,3,n] and checkSS=true (default), check if V is truly skew
+% symmetric.
+% R is returned as a 3-3-n matrix
 
-if length(V) ~= 3
-    error('input vector size is not 3');
+% check dimensions and skew-symmetric
+if size(S,1)==3 && size(S,2)==3
+    if ~exist('checkSS','var') || isempty(checkSS)
+        checkSS = true;
+    end
+    if checkSS
+        if ~isempty(find(S(1,1,:),1)) || ~isempty(find(S(2,2,:),1)) ||...
+            ~isempty(find(S(3,3,:),1)) ||...
+            ~isempty(find(S(1,2,:)+S(2,1,:),1)) ||...
+            ~isempty(find(S(1,3,:)+S(3,1,:),1)) ||...
+            ~isempty(find(S(2,3,:)+S(3,2,:),1))
+            error('the first two dimension of input must be skew symmetric');
+        end
+    end
+elseif size(S,1)==3 && size(S,3)==1
+    S = hat(S);
+elseif size(S,2)==3 && size(S,3)==1
+    S = hat(S);
+else
+    error('Input must be of size 3-3-n, 3-n-1, or n-3-1');
 end
 
-fi = norm(V);
-if fi == 0
-    R = eye(3);
-else
-    u = V/fi;
-    uSkew = skew(u);
-    R = eye(3) + sin(fi)*uSkew + (1-cos(fi))*uSkew^2;
+% calculate exponential map
+N = size(S,3);
+R = zeros(3,3,N);
+for n = 1:N
+    normV = sqrt(S(1,2,n)^2+S(1,3,n)^2+S(2,3,n)^2);
+    if normV==0
+        R(:,:,n) = eye(3);
+    else
+        R(:,:,n) = eye(3)+sin(normV)/normV*S(:,:,n)+...
+            (1-cos(normV))/normV^2*S(:,:,n)^2;
+    end
 end
 
 end
--- rotation3d/invQuat.m
+++
@@ -1,9 +0,0 @@
-function [ invQ ] = invQuat( Q )
-% inverse of a quaternion
-% Reference: Joan Sola, Quaternion kinematics for the error-state Kalman filter, 2017
-% Last edited by Weixin Wang, December 18, 2018
-
-invQ = [Q(1);-Q(2);-Q(3);-Q(4)]/norm(Q)^2;
-
-end
-
