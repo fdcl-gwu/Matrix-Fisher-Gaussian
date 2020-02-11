11ae49cc3ab0eac5fdc073a4a2907b5aa5a06293
weixinwang <weixinwang442@gmail.com>
2020-02-11 14:47:57 -0500

    two conventions for MLE; "MF_pdf_sampling" can be used with negative s3

--- SO3-real distribution/MFGMLEAppro.m
+++ SO3-real distribution/MFGMLEAppro.m
@@ -1,4 +1,4 @@
-function [ Miu, Sigma, P, U, S, V ] = MFGMLEAppro( x, R, w )
+function [ Miu, Sigma, P, U, S, V ] = MFGMLEAppro( x, R, w, convention )
 % let x be N-by-Ns, R be 3-by-3-by-Ns
 
 filePath = mfilename('fullpath');
@@ -10,10 +10,15 @@ if ~any(strcmp(pathCell,getAbsPath('..\rotation3d',filePath)))
     addpath(getAbsPath('..\rotation3d',filePath));
 end
 
+% default SVD convention is psvd
+if ~exist('convention','var') && isempty(convention)
+    convention = 1;
+end
+
 N = size(x,1);
 Ns = size(R,3);
 
-if ~exist('w','var')
+if ~exist('w','var') || isempty(w)
     w = ones(1,Ns)/Ns;
 end
 
@@ -27,36 +32,36 @@ for ns = 1:Ns
 end
 
 % Matrix Fisher part
-[Up,Dp,Vp] = usvd(ER,true);
-Sp = diag(pdf_MF_M2S(diag(Dp)));
-[U,S,V] = usvd(Up*Sp*Vp');
-
-% f(eta)
-fEta = zeros(3,Ns);
-for ns = 1:Ns
-    expEta = U'*R(:,:,ns)*V;
-    fEta(1,ns) = trace(S*expEta'*hat([1,0,0]));
-    fEta(2,ns) = trace(S*expEta'*hat([0,1,0]));
-    fEta(3,ns) = trace(S*expEta'*hat([0,0,1]));
+[U,D,V] = psvd(ER);
+S = diag(pdf_MF_M2S(diag(D)));
+if convention==2
+    if S(3,3)<0
+        S(:,1:2) = -S(:,1:2);
+        U(:,1:2) = -U(:,1:2);
+    end
 end
 
-% other empirical moments
-EfEta = sum(fEta.*w,2);
+% vR
+Q = mulRot(mulRot(U',R,0),V,0);
+vR = vee(mulRot(Q,S,0)-mulRot(S,permute(Q,[2,1,3]),0));
 
-covxfEta = zeros(N,N);
-covfEtafEta = zeros(N,N);
+% other empirical moments
+EvR = sum(vR.*w,2);
+
+covxvR = zeros(N,N);
+covvRvR = zeros(N,N);
 for ns = 1:Ns
-    covxfEta = covxfEta+(x(:,ns)-Ex)*(fEta(:,ns)-EfEta)'*w(ns);
-    covfEtafEta = covfEtafEta+(fEta(:,ns)-EfEta)*(fEta(:,ns)-EfEta)'*w(ns);
+    covxvR = covxvR+(x(:,ns)-Ex)*(vR(:,ns)-EvR)'*w(ns);
+    covvRvR = covvRvR+(vR(:,ns)-EvR)*(vR(:,ns)-EvR)'*w(ns);
 end
 
 % correlation part
-P = covxfEta*covfEtafEta^-1;
+P = covxvR*covvRvR^-1;
 
 % Gaussian part
 SigmaTilde2Inv = diag([S(2,2)+S(3,3),S(1,1)+S(3,3),S(1,1)+S(2,2)]);
-Miu = Ex-P*EfEta;
-Sigma = covxx-covxfEta*covfEtafEta^-1*covxfEta'+...
+Miu = Ex-P*EvR;
+Sigma = covxx-covxvR*covvRvR^-1*covxvR'+...
     P*SigmaTilde2Inv*P';
 
 if ~any(strcmp(pathCell,getAbsPath('Matrix-Fisher-Distribution',filePath)))
@@ -65,13 +70,6 @@ end
 if ~any(strcmp(pathCell,getAbsPath('..\rotation3d',filePath)))
     rmpath(getAbsPath('..\rotation3d',filePath));
 end
-
-end
-
-
-function [ vec ] = mat2vec( mat )
-
-vec = [mat(1,:)'; mat(2,:)'; mat(3,:)'];
 
 end
 
