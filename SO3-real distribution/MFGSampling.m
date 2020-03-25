edf84c1ba459d7b670f733f5ca1ba1df3d79e4be
weixinwang <weixinwang442@gmail.com>
2020-03-25 14:12:58 -0400

    15 state uncertainty propagation

--- SO3-real distribution/MFGSampling.m
+++ SO3-real distribution/MFGSampling.m
@@ -19,7 +19,7 @@ SigmaMInv = diag([S(2,2)+S(3,3),S(1,1)+S(3,3),S(1,1)+S(2,2)]);
 Sigmac = Sigma-P*SigmaMInv*P';
 
 R = zeros(3,3,Ns);
-x = zeros(3,Ns);
+x = zeros(N,Ns);
 for ns = 1:Ns
     R(:,:,ns) = U*Q(:,:,ns)*V';
     gR = vee(Q(:,:,ns)*S-S*Q(:,:,ns)');
