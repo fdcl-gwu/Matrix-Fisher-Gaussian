2a54a72c3c5ccc13939360077d83f766a70cb412
weixinwang <weixinwang442@gmail.com>
2021-09-26 10:40:03 -0400

    writings for pre-integration

---
+++ SO3-real distribution/Filter/Comp.m
@@ -0,0 +1,66 @@
+function [ R, x, stepT ] = Comp( gyro, Mea, sf, parameters )
+
+N = size(gyro,2);
+dt = 1/sf;
+
+%% settings
+% angular velocity noise parameters
+if exist('parameters','var') && isfield(parameters,'omegaNoise')
+    randomWalk = parameters.omegaNoise.randomWalk;
+    biasInstability = parameters.omegaNoise.biasInstability;
+else
+    randomWalk = 10*pi/180;
+    biasInstability = 500/3600*pi/180;
+end
+
+% measurement noise parameters
+if exist('parameters','var') && isfield(parameters,'meaNoise')
+    meaNoise = parameters.meaNoise;
+else
+    meaNoise = 0.2^2*eye(3);
+end
+
+% other settings
+if exist('parameters','var') && isfield(parameters,'setting')
+    omegaLocal = parameters.setting.omegaLocal;
+    GaussMea = parameters.setting.GaussMea;
+    meaIsVec = parameters.setting.meaIsVec;
+else
+    omegaLocal = true;
+    GaussMea = true;
+    meaIsVec = false;
+end
+
+if meaIsVec
+    if exist('parameters','var') && isfield(parameters,'setting')
+        vecRefInertial = parameters.setting.vecRefInertial;
+        nVecRef = parameters.setting.nVecRef;
+        vRef = parameters.setting.vRef;
+    else
+        vecRefInertial = true;
+        nVecRef = 1;
+        vRef = [0;0;1];
+    end
+else
+    if exist('parameters','var') && isfield(parameters,'setting')
+        attMeaLocal = parameters.setting.attMeaLocal;
+    else
+        attMeaLocal = true;
+    end
+end
+
+% convert noise distributions
+if meaIsVec
+    if ~GaussMea
+        for nv = 1:nVecRef
+            meaNoise(nv) = VM2Gau(meaNoise(nv));
+        end
+    end
+else
+    if ~GaussMea
+        meaNoise = MF2Gau(meaNoise);
+    end
+end
+
+end
+
